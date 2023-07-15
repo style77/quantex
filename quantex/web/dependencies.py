2c2a5ad6d580d414b5c74243c746b813c57f91a6
style77 <stylek777@gmail.com>
2023-07-15 08:48:53 +0200

    fix: get secret key from settings

--- quantex/web/dependencies.py
+++ quantex/web/dependencies.py
@@ -2,9 +2,9 @@ from typing import Annotated
 
 from fastapi import Header, HTTPException
 
-from quantex.settings import Settings
+from quantex.settings import settings
 
 
 async def verify_secret(x_secret: Annotated[str, Header()]):
-    if x_secret != Settings.secret_key:
+    if x_secret != settings.secret_key:
         raise HTTPException(status_code=403, detail="Secret header invalid")
