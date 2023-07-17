b063b0d635286e8e935f779575bdcbecafe27046
style <stylek777@gmail.com>
2023-07-17 16:08:40 +0200

    feat: listener and telegram bot base

---
+++ quantex-scraper/listener.py
@@ -0,0 +1,16 @@
+class EventListener:
+    def __init__(self):
+        self.listeners = {}
+
+    def on(self, event):
+        def decorator(func):
+            if event not in self.listeners:
+                self.listeners[event] = []
+            self.listeners[event].append(func)
+            return func
+        return decorator
+
+    def call(self, event, *args, **kwargs):
+        if event in self.listeners:
+            for listener in self.listeners[event]:
+                listener(*args, **kwargs)
---
+++ quantex-scraper/telegram.py
@@ -0,0 +1,13 @@
+# This is my implementation for the Telegram API
+import requests
+
+
+class TelegramBot:
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+    def send_message(self, chat: str, text: str):
+        data = {"chat_id": chat, "text": text}
+        r = requests.post(self.api_url + "sendMessage", data=data)
+        return r.json()
